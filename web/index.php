100ee96f40f8b543f23dd98ecea6b4358a0f71a8
Bernd Arnold <bernd_a@gmx.de>
2007-12-11 20:26:45 +0000

    Added zip file to download list, download list as array now

--- web/index.php
+++ web/index.php
@@ -6,10 +6,24 @@ header("Vary: Accept");
 #else
 #    header("Content-Type: text/html; charset=utf-8");
 
-$downloadurl = "http://downloads.sourceforge.net/bombermaaan/Bombermaaan_1.02_Setup.exe?modtime=1197055380&big_mirror=0";
-$filemd5 = "4a708baed1d671be6ff0ba14bcd851e2";
-$filesha1 = "d57361ee3c83f4cd03bee0d881f14a02b6a953ad";
-	
+$downloads = array(
+	array(
+	"http://downloads.sourceforge.net/bombermaaan/Bombermaaan_1.02_Setup.exe?modtime=1197055380&big_mirror=0",
+	"Installer for Win32 - Version 1.02",
+	"4a708baed1d671be6ff0ba14bcd851e2", // MD5
+	"d57361ee3c83f4cd03bee0d881f14a02b6a953ad"  // SHA1
+	),
+	array(
+	"http://downloads.sourceforge.net/bombermaaan/Bombermaaan_1.02.zip?modtime=1197405778&big_mirror=0",
+	"Zip file for Win32 - Version 1.02",
+	),
+	array(
+	"http://downloads.sourceforge.net/bombermaaan/Bombermaaan_102_src.zip?modtime=1083110400&big_mirror=0",
+	"Source files - Version 1.02",
+	),
+);
+
+
 ?>
 <?php echo '<?' . 'xml version="1.0" encoding="UTF-8"?>' . "\n"; ?>
 <?php echo '<!' . 'DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">' . "\n"; ?>
@@ -53,8 +67,27 @@ Download the files from here:
 </p>
 
 <ul>
-<li><a href="<?php echo htmlentities( $downloadurl ); ?>">Installer for Win32 - Version 1.02</a><br /><small>MD5:&nbsp;<?php echo $filemd5; ?>, SHA1:&nbsp;<?php echo $filesha1; ?></small></li>
-<li><a href="http://downloads.sourceforge.net/bombermaaan/Bombermaaan_102_src.zip?modtime=1083110400&amp;big_mirror=0">Source files - Version 1.02</a></li>
+<?php
+
+foreach ( $downloads as $item ) {
+
+	$url = $item[ 0 ];
+	$text = $item[ 1 ];
+	$md5 = $item[ 2 ];
+	$sha1 = $item[ 3 ];
+
+?>	
+<li>
+<a href="<? echo htmlentities( $url ); ?>"><?php echo htmlentities( $text ); ?></a>
+<?php if ( ! empty( $md5 ) ) { echo "<br /><small>MD5:&nbsp;$md5</small>"; } ?>
+<?php if ( ! empty( $sha1 ) ) { echo "<br /><small>SHA1:&nbsp;$sha1</small>"; } ?>
+</li>
+
+<?
+
+}  // foreach ( ... )
+
+?>
 </ul>
 
 <p>
@@ -62,6 +95,10 @@ The installer was tested on Win XP with ServicePack 2 and Win Vista. Please note
 You need administrative rights for installing Bombermaaan. This is considered to be changed in the future.
 <br />
 The uninstaller was tested on Win XP with ServicePack 2 and Win Vista.
+</p>
+
+<p>
+If you don't want to use the installer, you can also download the Zip file and unzip the folder somewhere and run Bombermaaan.exe from there. Please ensure to unzip the whole directory, not only Bombermaaan.exe.
 </p>
 
 </div>

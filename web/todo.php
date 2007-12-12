3b9618ebafeeacf00fc638c71fd348f6ed1a347e
Bernd Arnold <bernd_a@gmx.de>
2007-12-12 15:58:53 +0000

    Removed unnecessary downloads array

--- web/todo.php
+++ web/todo.php
@@ -9,24 +9,6 @@ header("Vary: Accept");
 #else
 #    header("Content-Type: text/html; charset=utf-8");
 
-$downloads = array(
-	array(
-	"http://downloads.sourceforge.net/bombermaaan/Bombermaaan_1.02_Setup.exe?modtime=1197055380&big_mirror=0",
-	"Installer for Win32 - Version 1.02",
-	"4a708baed1d671be6ff0ba14bcd851e2", // MD5
-	"d57361ee3c83f4cd03bee0d881f14a02b6a953ad"  // SHA1
-	),
-	array(
-	"http://downloads.sourceforge.net/bombermaaan/Bombermaaan_1.02.zip?modtime=1197405778&big_mirror=0",
-	"Zip file for Win32 - Version 1.02",
-	),
-	array(
-	"http://downloads.sourceforge.net/bombermaaan/Bombermaaan_102_src.zip?modtime=1083110400&big_mirror=0",
-	"Source files - Version 1.02",
-	),
-);
-
-
 ?>
 <?php echo '<?' . 'xml version="1.0" encoding="UTF-8"?>' . "\n"; ?>
 <?php echo '<!' . 'DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">' . "\n"; ?>

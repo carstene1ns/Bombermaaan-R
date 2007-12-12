8c02b5b6cd3015b8732efb0696c10ec23a5fd074
Bernd Arnold <bernd_a@gmx.de>
2007-12-12 15:51:36 +0000

    Added text and link

--- web/license.php
+++ web/license.php
@@ -24,7 +24,22 @@ $webpage->head();
 
 <div id="content">
 
-Bombermaaan license: GPL
+<pre>
+Bombermaaan is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Bombermaaan is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+</pre>
+
+<p>
+Read the <a href="http://www.gnu.org/licenses/gpl.html">GNU General Public License</a>.
+</p>
 
 </div>
 

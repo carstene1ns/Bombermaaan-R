3331a3f87a4ead3c49366027a8eda2eb0a4c0fe9
Bernd Arnold <bernd_a@gmx.de>
2007-12-12 20:03:40 +0000

    Removed old page text, added some text from Thibaut's original site

--- web/index.php
+++ web/index.php
@@ -42,127 +42,13 @@ $webpage->head();
 
 <div id="content">
 
-<div style="float: left; width: 40%;">
+<h2>Introduction</h2>
 
-<p>
-You may want to view ...
-</p>
+Bombermaaan is a classic bomberman game. Thibaut Tollemer started this
+project on July 2000. He's been using this project as a demo of what he 
+could do as a game programmer, in order to be hired.
 
-<ul>
-<li>the <a href="http://fury94.free.fr/">current project page</a></li>
-<li>the <a href="https://sourceforge.net/projects/bombermaaan/">project page on sourceforge.net</a></li>
-<li>the <a href="https://sourceforge.net/project/showfiles.php?group_id=81520">download section on sourceforge.net</a></li>
-<li>the <a href="http://bombermaaan.svn.sourceforge.net/viewvc/bombermaaan/">subversion repository</a> on sourceforge.net</li>
-<li>the <a href="http://bombermaaan.svn.sourceforge.net/viewvc/*checkout*/bombermaaan/trunk/docs/Readme.html?revision=5&amp;pathrev=5">README file</a> (very latest version out of the subversion repository)</li>
-</ul>
-
-<p>
-Download the files from here:
-</p>
-
-<ul>
-<?php
-
-foreach ( $downloads as $item ) {
-
-	$url = $item[ 0 ];
-	$text = $item[ 1 ];
-	$md5 = $item[ 2 ];
-	$sha1 = $item[ 3 ];
-
-?>	
-<li>
-<a href="<? echo htmlentities( $url ); ?>"><?php echo htmlentities( $text ); ?></a>
-<?php if ( ! empty( $md5 ) ) { echo "<br /><small>MD5:&nbsp;$md5</small>"; } ?>
-<?php if ( ! empty( $sha1 ) ) { echo "<br /><small>SHA1:&nbsp;$sha1</small>"; } ?>
-</li>
-
-<?
-
-}  // foreach ( ... )
-
-?>
-</ul>
-
-<p>
-The installer was tested on Win XP with ServicePack 2 and Win Vista. Please note, if you install Bombermaaan on Vista to the default program files directory, the configuration and log file cannot be created. However, you can play the game. But the settings weren't remembered when you start the game the next time. To avoid this, you can install Bombermaaan to a different directory, for example &quot;C:\Bombermaaan&quot;.<br />
-You need administrative rights for installing Bombermaaan. This is considered to be changed in the future.
-<br />
-The uninstaller was tested on Win XP with ServicePack 2 and Win Vista.
-</p>
-
-<p>
-If you don't want to use the installer, you can also download the Zip file and unzip the folder somewhere and run Bombermaaan.exe from there. Please ensure to unzip the whole directory, not only Bombermaaan.exe.
-</p>
-
-</div>
-
-<div>
-
-<p>
-
-<img style="padding-left: 2em;" src="screenshots/screen1.png" alt="Screenshot #1" />
-<img style="padding-left: 2em;" src="screenshots/screen7.png" alt="Screenshot #2" />
-<img style="padding-left: 2em;" src="screenshots/screen8.png" alt="Screenshot #3" />
-
-</p>
-
-</div>
-
-<div style="clear: both;">
-
-<h2 style="margin-top: 5em;">Todo list</h2>
-
-<table>
-
-<tr>
-<th>Item</th>
-<th>Progress</th>
-<th>Status</th>
-<th>Description</th>
-<th>Added</th>
-<th>Last changed</th>
-</tr>
-
-<tr>
-<td>Blocks where bombs start moving/change direction</td>
-<td>0 %</td>
-<td>Planned</td>
-<td>There are certain blocks in the arena. When a bomber puts a bomb there or when a moving bomb arrives there, the bomb begins moving or changes the direction.</td>
-<td>2007-12-10</td>
-<td>2007-12-10</td>
-</tr>
-
-<tr>
-<td>Skins</td>
-<td>0 %</td>
-<td>Planned</td>
-<td>Skins with different images.</td>
-<td>2007-12-10</td>
-<td>2007-12-10</td>
-</tr>
-
-<tr>
-<td>More than two keyboard inputs</td>
-<td>0 %</td>
-<td>Planned</td>
-<td>Since you can plug many USB keyboards, it could be possible to have five human players in the same game.</td>
-<td>2007-12-10</td>
-<td>2007-12-10</td>
-</tr>
-
-<tr>
-<td>Team mode</td>
-<td>0 %</td>
-<td>Planned</td>
-<td>Players can be assigned to teams. The match is finished if there is only one team alive. In addition, there could be an option if items are shared in a team.</td>
-<td>2007-12-11</td>
-<td>2007-12-11</td>
-</tr>
-
-</table>
-
-</div>
+As he was hired, he published the source code and created a project space on sourceforge.net.
 
 </div>
 

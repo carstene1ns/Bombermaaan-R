2d649a0484f4d1baf1daf48046644a722c5c927c
Bernd Arnold <bernd_a@gmx.de>
2007-12-13 19:45:18 +0000

    Moved http, xml and doctype header to inc/webpage.php

--- web/inc/webpage.php
+++ web/inc/webpage.php
@@ -4,6 +4,14 @@ class WebPage {
 
 	function head() {
 	
+		header( "Vary: Accept" );
+		#if (stristr($_SERVER["HTTP_ACCEPT"], "application/xhtml+xml")) 
+		    header( "Content-Type: application/xhtml+xml; charset=utf-8" );
+		#else
+		#    header("Content-Type: text/html; charset=utf-8");
+
+		echo '<?' . 'xml version="1.0" encoding="UTF-8"?>' . "\n";
+		echo '<!' . 'DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">' . "\n";
 		?>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
 <head>

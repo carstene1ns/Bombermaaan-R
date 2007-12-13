e581350e10e5c02602870549f506e8c16d837b79
Bernd Arnold <bernd_a@gmx.de>
2007-12-13 20:00:07 +0000

    Added quotations around programs path (minor change, just for my vista environment: changed path to image file)

--- images/Convert_svg2png.cmd
+++ images/Convert_svg2png.cmd
@@ -1,8 +1,8 @@
 @echo off
 
-set imgpath=C:\Dokumente und Einstellungen\Bernd\Eigene Dateien\Bombermaaan\repository_trunk\images
+set imgpath=C:\Users\Bernd\Documents\Bombermaaan\subversion_trunk\images
 
 rem --export-area-drawing 
 rem --export-dpi=150
-%ProgramFiles%\Inkscape\inkscape.exe "%imgpath%\Title.svg" --export-png="%imgpath%\Title_convertedsvg.png"
+"%ProgramFiles%\Inkscape\inkscape.exe" "%imgpath%\Title.svg" --export-png="%imgpath%\Title_convertedsvg.png"
 

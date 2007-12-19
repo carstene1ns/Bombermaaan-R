df59193a26ee23c22dc25996e40044d7b04a038e
Bernd Arnold <bernd_a@gmx.de>
2007-12-19 14:53:36 +0000

    Added row (item amount in walls/bomber skills)

--- web/todo.php
+++ web/todo.php
@@ -28,6 +28,15 @@ This list shows the ideas we had and don't want to forget.
 </tr>
 
 <tr>
+<td>Adjustable items in walls and bomber skills on a per-level basis</td>
+<td>70 %</td>
+<td>In Progress</td>
+<td>The amount of items in walls and the initial bomber skills can set in each level file.<br />Tracker items 1847047 and 1847048.</td>
+<td>2007-12</td>
+<td>2007-12-18</td>
+</tr>
+
+<tr>
 <td>Blocks where bombs start moving/change direction</td>
 <td>0 %</td>
 <td>Noticed</td>

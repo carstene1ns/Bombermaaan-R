2c7d000fd60e478339ffc597cda68d04307d4aa6
Bernd Arnold <bernd_a@gmx.de>
2007-12-28 20:10:34 +0000

    Replaced fixed imgpath setting by variable, added copyright notice

--- images/Convert_svg2png.cmd
+++ images/Convert_svg2png.cmd
@@ -1,6 +1,13 @@
 @echo off
 
-set imgpath=C:\Users\Bernd\Documents\Bombermaaan\subversion_trunk\images
+rem ****************************************
+rem *** This file is part of Bombermaaan ***
+rem *** Copyright (C) 2007 Bernd Arnold  ***
+rem ****************************************
+
+
+rem Get the current directory
+set imgpath=%CD%
 
 rem --export-area-drawing 
 rem --export-dpi=150

7db9b2677e971f2ec075f452b86b97a214da687f
Bernd Arnold <bernd_a@gmx.de>
2008-01-03 01:44:39 +0000

    Added operating systems info, added link to how-to-get-it-compiled guide

--- web/development.php
+++ web/development.php
@@ -15,7 +15,7 @@ $webpage->head();
 <h2>Development</h2>
 
 <p>
-Bombermaaan is written in C++ and hosted on SourceForge.net.
+Bombermaaan is written in C++ and hosted on SourceForge.net. Bombermaaan should run on all 32-bit MS Windows operating systems.
 </p>
 
 <h2>Links to SourceForge.net</h2>
@@ -29,7 +29,7 @@ Bombermaaan is written in C++ and hosted on SourceForge.net.
 <h2>Compile Bombermaaan by yourself</h2>
 
 <p>
-There is a (incomplete so far) how-to guide you can use if you want to compile Bombermaaan by yourself.
+There is a (incomplete so far) <a href="http://bombermaaan.svn.sourceforge.net/viewvc/*checkout*/bombermaaan/trunk/docs/how-to-compile.txt?revision=152">how-to guide</a> you can use if you want to compile Bombermaaan by yourself.
 Unfortunately, the image and sound files are not in the subversion repository yet. You can get them from the source package, though.
 This is true for the small sprites (16 pixels per block). You can delete the RES32 project from your VC++ solution to get Bombermaaan compiled.
 The RES32 project generates the DLL for the larger sprites (32 pixels per block).

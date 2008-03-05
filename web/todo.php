bf0b35d516a2c14b51ce424a37bf1f240a0f78eb
Bernd Arnold <bernd_a@gmx.de>
2008-03-05 16:26:31 +0000

    Updated last changed date

--- web/todo.php
+++ web/todo.php
@@ -42,7 +42,7 @@ This list shows the ideas we had and don't want to forget.
 <td>In Progress</td>
 <td>There are certain blocks in the arena. When a bomber puts a bomb there or when a moving bomb arrives there, the bomb begins moving or changes the direction. Tracker item <a href="https://sourceforge.net/tracker/index.php?func=detail&amp;aid=1908105&amp;group_id=81520&amp;atid=563213">1908105</a>.</td>
 <td>2007-12-10</td>
-<td>2007-12-10</td>
+<td>2008-03-05</td>
 </tr>
 
 <tr>

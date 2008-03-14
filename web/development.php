ad78444e22cc8a86b456bdf0a64caf4c7398a94d
Bernd Arnold <bernd_a@gmx.de>
2008-03-14 16:33:07 +0000

    Added license file item to list

--- web/development.php
+++ web/development.php
@@ -61,6 +61,7 @@ Zip package
 <li>Create a new folder <code>Bombermaaan_<i>VERSION</i>.zip</code></li>
 <li>Place the files <code>Bombermaaan.exe</code>, <code>Bombermaaan.dll</code> and <code>FMOD.dll</code> in this directory</li>
 <li>Add the readme file <code>Readme.html</code></li>
+<li>Add the license file <code>COPYING.txt</code></li>
 <li>Copy the directory with level files in the new folder</li>
 <li>Zip the folder</li>
 </ul>

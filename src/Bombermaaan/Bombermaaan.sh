670ebb70d5e7c5f471c7b7e6befc32ecba6085e9
eggomat <eggomat@localhost>
2008-09-18 11:03:11 +0000

    Added / Removed files who were mentioned in the log of rev 519

--- src/Bombermaaan/Bombermaaan.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd /home/otto/Projects/Bombermaaan/BOMBERMAN
-./Bomberman
---
+++ src/checkLineBreaks.pl
@@ -0,0 +1,50 @@
+#!/usr/bin/perl
+
+# This small scripts checks if all source files have the Windows Linebreaks
+# Only tested in Linux
+my $directory = "Bombermaaan";
+my $rep;
+
+opendir (DIR, $directory) or die "Source directory not found.";
+	@files = readdir(DIR);
+closedir (DIR);
+
+foreach $file (@files)
+{
+	if (lc($file) =~ /\.cpp$/ || lc($file) =~ /\.h$/)
+	{
+		# read file
+		open (FILE, "$directory/$file") or die "Could not open $directory/$file for reading\n";
+			@lines = <FILE>;
+		close(FILE);
+
+		# count how many changes have to be made
+		$rep = 0;
+		foreach $line (@lines)
+		{
+			if ($line !~ /\r\n$/ && $line =~ /\n$/)
+			{
+				$rep++;
+			}
+			print OUT $line;
+		}
+
+		# write file
+		if ($rep > 0)
+		{
+			open (OUT, ">$directory/$file") or die "Could not open $directory/$file for writing\n";
+				foreach $line (@lines)
+				{
+					if ($line !~ /\r\n$/ && $line =~ /\n$/)
+					{
+						$line =~ s/\n$/\r\n/;
+					}
+					print OUT $line;
+				}
+			close (OUT);
+
+			print "Edited $file. There were $rep replacements.\n";
+		}
+	}
+}
+

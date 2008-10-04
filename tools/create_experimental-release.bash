a842895b9d9702913d356aaf577c88d1d813ef10
Bernd Arnold <bernd_a@gmx.de>
2008-10-04 10:31:54 +0000

    Added check of CGame.cpp last modification time, added creation of source code folder with zip and tar.gz file

--- tools/create_experimental-release.bash
+++ tools/create_experimental-release.bash
@@ -67,8 +67,15 @@ REVISION=`grep '^#define BOMBERMAAAN_BUILD_STRING ' ../src/Bombermaaan/CGame.cpp
 VERSION=`grep '^#define BOMBERMAAAN_VERSION_STRING ' ../src/Bombermaaan/CGame.cpp | sed -e 's/.* "//' -e 's/".*//'`
 
 # The 'e' means experimental
-TARGETDIR=./output/Bombermaaan_${VERSION}.${REVISION}e_win32
-TARGETZIP=./output/Bombermaaan_${VERSION}.${REVISION}e_win32.zip
+BASENAME=Bombermaaan_${VERSION}.${REVISION}e_`date '+%Y%m%d'`
+TARGETDIR=${BASENAME}_win32
+TARGETZIP=${BASENAME}_win32.zip
+SRCTARGETDIR=${BASENAME}_src
+SRCTARGETGZ=${BASENAME}_src.tar.gz
+SRCTARGETZIP=${BASENAME}_src.zip
+
+# Makes friendly names in tar packages when additional directories are skipped from the SRCTARGETDIR variable
+cd ./output/.       || error "Cd ended with return code $?."
 
 
 #
@@ -83,6 +90,16 @@ if [ -z "$VERSION" ] ; then
     error "Could not find version string."
 fi
 
+# The build and version strings have to be updated in the file CGame.cpp
+# This check ensures the last check was less then 10 minutes (600 seconds) ago so we don't forget the update
+LASTCHG=`date '+%s' -r ../../src/Bombermaaan/CGame.cpp`
+NOW=`date '+%s'`
+let "AGE=NOW-LASTCHG"
+
+if [ "$AGE" -gt 600 ] ; then
+    error "File CGame.cpp wasn't modified recently (last update $AGE seconds ago, see BOMBERMAAAN_BUILD_STRING and BOMBERMAAAN_VERSION_STRING)."
+fi
+
 if [ -e "$TARGETDIR" ] ; then
     error "Target folder '$TARGETDIR' already exists."
 fi
@@ -91,8 +108,16 @@ if [ -e "$TARGETZIP" ] ; then
     error "Target zip package '$TARGETZIP' already exists."
 fi
 
-if [ ! -d ./output ] ; then
-    error "Output folder doesn\'t exist. Maybe we are in the wrong directory."
+if [ -e "$SRCTARGETDIR" ] ; then
+    error "Target folder '$SRCTARGETDIR' already exists."
+fi
+
+if [ -e "$SRCTARGETZIP" ] ; then
+    error "Target zip package '$SRCTARGETZIP' already exists."
+fi
+
+if [ -e "$SRCTARGETGZ" ] ; then
+    error "Target zip package '$SRCTARGETGZ' already exists."
 fi
 
 
@@ -102,22 +127,22 @@ fi
 
 echo Creating $RELEASETYPE release package folder for version $VERSION, rev. $REVISION now...
 
-mkdir "$TARGETDIR"                                                          || error "Mkdir ended with return code $?."
+mkdir "$TARGETDIR"                                                              || error "Mkdir ended with return code $?."
 
-mkdir "$TARGETDIR/Levels"                                                   || error "Mkdir ended with return code $?."
-cp ../src/_Test_/Release/Levels/L*.TXT "$TARGETDIR/Levels/."                || error "Copy ended with return code $?."
+mkdir "$TARGETDIR/Levels"                                                       || error "Mkdir ended with return code $?."
+cp ../../src/_Test_/Release/Levels/L*.TXT "$TARGETDIR/Levels/."                 || error "Copy ended with return code $?."
 
-cp ../src/_Test_/Release/Bombermaaan.exe        "$TARGETDIR/."              || error "Copy ended with return code $?."
-cp ../src/_Test_/Release/Bombermaaan_32.dll        "$TARGETDIR/."           || error "Copy ended with return code $?."
-cp ../src/CHANGELOG.txt        "$TARGETDIR/."                               || error "Copy ended with return code $?."
-cp ../COPYING.txt        "$TARGETDIR/."                                     || error "Copy ended with return code $?."
-cp ../docs/Readme.html        "$TARGETDIR/."                                || error "Copy ended with return code $?."
+cp ../../src/_Test_/Release/Bombermaaan.exe        "$TARGETDIR/."               || error "Copy ended with return code $?."
+cp ../../src/_Test_/Release/Bombermaaan_32.dll        "$TARGETDIR/."            || error "Copy ended with return code $?."
+cp ../../src/CHANGELOG.txt        "$TARGETDIR/."                                || error "Copy ended with return code $?."
+cp ../../COPYING.txt        "$TARGETDIR/."                                      || error "Copy ended with return code $?."
+cp ../../docs/Readme.html        "$TARGETDIR/."                                 || error "Copy ended with return code $?."
 
-cp ../src/_Test_/Release/libogg*.dll            "$TARGETDIR/."              || error "Copy ended with return code $?."
-cp ../src/_Test_/Release/libvorbis*.dll         "$TARGETDIR/."              || error "Copy ended with return code $?."
-cp ../src/_Test_/Release/SDL*.dll               "$TARGETDIR/."              || error "Copy ended with return code $?."
+cp ../../src/_Test_/Release/libogg*.dll            "$TARGETDIR/."               || error "Copy ended with return code $?."
+cp ../../src/_Test_/Release/libvorbis*.dll         "$TARGETDIR/."               || error "Copy ended with return code $?."
+cp ../../src/_Test_/Release/SDL*.dll               "$TARGETDIR/."               || error "Copy ended with return code $?."
 #smpeg.dll not needed?
-#cp ../src/_Test_/Release/smpeg.dll              "$TARGETDIR/."         || error "Copy ended with return code $?."
+#cp ../../src/_Test_/Release/smpeg.dll              "$TARGETDIR/."         || error "Copy ended with return code $?."
 
 #
 # Creating zip file
@@ -125,7 +150,45 @@ cp ../src/_Test_/Release/SDL*.dll               "$TARGETDIR/."              || e
 
 echo Creating $RELEASETYPE release package for version $VERSION, rev. $REVISION now...
 
-/cygdrive/c/Programme/7-Zip/7z.exe a -tzip "$TARGETZIP" "$TARGETDIR"        || error "7-Zip ended with return code $?."
+/cygdrive/c/Programme/7-Zip/7z.exe a -tzip "$TARGETZIP" "$TARGETDIR"            || error "7-Zip ended with return code $?."
+
+#
+# Creating folder with source code
+#
+
+echo Creating $RELEASETYPE source release package folder for version $VERSION, rev. $REVISION now...
+
+mkdir "$SRCTARGETDIR"                                                           || error "Mkdir ended with return code $?."
+
+mkdir "$SRCTARGETDIR/Bombermaaan"                                               || error "Mkdir ended with return code $?."
+cp ../../src/Bombermaaan/*.cpp "$SRCTARGETDIR/Bombermaaan/."                    || error "Copy ended with return code $?."
+cp ../../src/Bombermaaan/*.h "$SRCTARGETDIR/Bombermaaan/."                      || error "Copy ended with return code $?."
+cp ../../src/Bombermaaan/Makefile "$SRCTARGETDIR/Bombermaaan/."                 || error "Copy ended with return code $?."
+cp ../../src/Bombermaaan/Makefile.win "$SRCTARGETDIR/Bombermaaan/."             || error "Copy ended with return code $?."
+
+mkdir "$SRCTARGETDIR/RES"                                                       || error "Mkdir ended with return code $?."
+cp ../../src/RES/*.cpp "$SRCTARGETDIR/RES/."                                    || error "Copy ended with return code $?."
+cp ../../src/RES/*.h "$SRCTARGETDIR/RES/."                                      || error "Copy ended with return code $?."
+cp ../../src/RES/Makefile.win "$SRCTARGETDIR/RES/."                             || error "Copy ended with return code $?."
+
+mkdir "$SRCTARGETDIR/RES32"                                                     || error "Mkdir ended with return code $?."
+cp ../../src/RES32/*.cpp "$SRCTARGETDIR/RES32/."                                || error "Copy ended with return code $?."
+cp ../../src/RES32/*.h "$SRCTARGETDIR/RES32/."                                  || error "Copy ended with return code $?."
+cp ../../src/RES32/Makefile.win "$SRCTARGETDIR/RES32/."                         || error "Copy ended with return code $?."
+
+cp ../../src/CHANGELOG.txt        "$SRCTARGETDIR/."                             || error "Copy ended with return code $?."
+cp ../../src/Makefile        "$SRCTARGETDIR/."                                  || error "Copy ended with return code $?."
+cp ../../COPYING.txt        "$SRCTARGETDIR/."                                   || error "Copy ended with return code $?."
+cp ../../docs/Readme.html        "$SRCTARGETDIR/."                              || error "Copy ended with return code $?."
+
+#
+# Creating zip files from source code folder
+#
+
+echo Creating $RELEASETYPE source release package for version $VERSION, rev. $REVISION now...
+
+/cygdrive/c/Programme/7-Zip/7z.exe a -tzip "$SRCTARGETZIP" "$SRCTARGETDIR"      || error "7-Zip ended with return code $?."
+tar czf "$SRCTARGETGZ" "$SRCTARGETDIR"        || error "Tar ended with return code $?."
 
 #
 # Checksums
@@ -133,9 +196,13 @@ echo Creating $RELEASETYPE release package for version $VERSION, rev. $REVISION 
 
 echo
 echo Checksum MD5:
-md5sum "$TARGETZIP" | sed -e 's!\./output/!!'        || error "Md5sum ended with return code $?."
+md5sum $BASENAME*.*       || error "Md5sum ended with return code $?."
 
 echo Checksum SHA1:
-sha1sum "$TARGETZIP" | sed -e 's!\./output/!!'       || error "Sha1sum ended with return code $?."
+sha1sum $BASENAME*.*      || error "Sha1sum ended with return code $?."
+
+#
+# End
+#
 
 echo Done.

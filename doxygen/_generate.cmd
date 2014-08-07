1937ce97380d5de3f43982891de39e33c2112708
Carsten Teibes <dev@f4ke.de>
2014-08-07 20:07:56 +0200

    update doxygen configuration

--- doxygen/_generate.cmd
+++
@@ -1,3 +0,0 @@
-if exist "\Software\doxygen\bin\doxygen.exe" set doxygen="\Software\doxygen\bin\doxygen.exe"
-if exist "%ProgramFiles%\doxygen\bin\doxygen.exe" set doxygen="%ProgramFiles%\doxygen\bin\doxygen.exe"
-%doxygen% Doxyfile.txt
